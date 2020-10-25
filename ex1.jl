8c6a8e62edb3ff915be47d271db287d14c5d3375
jeniok360v <gerhugwsif@gmail.com>
2020-10-25 05:16:25 +0100

    ex1.jl not iterative

--- ex1.jl
+++ ex1.jl
@@ -1,1 +1,19 @@
-println("Hell of world")
+println("nextfloat(convert(T,1.0))-(convert(T,1.0)), gdzie T - typ")
+for T in [Float16, Float32, Float64]
+	myeps=nextfloat(convert(T,1.0))-(convert(T,1.0))
+	diff = myeps-eps(T)
+	println("$(lpad(T, 7)): $myeps")
+end
+
+println("Wbudowana funkcja eps(T)")
+for T in [Float16, Float32, Float64]
+	println("$(lpad(T, 7)): $(eps(T))")
+end
+
+println("Roznica:")
+for T in [Float16, Float32, Float64]
+	myeps=nextfloat(convert(T,1.0))-(convert(T,1.0))
+	diff = myeps-eps(T)
+	println("$(lpad(T, 7)): $(diff)")
+end
+
